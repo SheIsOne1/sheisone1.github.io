d8e1420c85515872a56d5c46c2f76b79ab748bca
sheisone <shiriarbiv@mail.tau.ac.il>
2025-01-04 19:49:09 +0200

    changes

--- twist/task 7/taskseven.js
+++ twist/task 7/taskseven.js
@@ -2,6 +2,14 @@ function sayHi () {
     alert("Hi there!");
 }
 function logHi () {
-    console.log ("Hello there!");
-    
+    console.log ("Hello there!");  
 }
+
+function moveDorothy() {
+    const dorothy =document.getElementById ('dorothy');
+    dorothy.style.left = '300px';  
+    dorothy.style.top = '400px';  
+
+
+}
+
