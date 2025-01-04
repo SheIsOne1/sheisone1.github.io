b5f8dc6fa9e8e472b41621bc8d0735df3a18bb75
sheisone <shiriarbiv@mail.tau.ac.il>
2025-01-04 21:20:48 +0200

    keep walking animation

--- twist/task 7/taskseven.js
+++ twist/task 7/taskseven.js
@@ -7,8 +7,7 @@ function logHi () {
 
 
 function moveDorothy(){
-    document.getElementById ('dorothy');
-    dorothy.style.animation = 'walkAnimation 1s steps(10) infinite';
+    document.getElementById ('dorothy').style.animation='walkAnimation 1s steps(10) infinite';
     dorothy.style.left = '300px';  
     dorothy.style.top = '400px';  
 
